8cbd444466493e121d2073df7bc861b1da0a454a
y2k <itwisterlx@gmail.com>
2023-06-17 16:23:55 +0200

    fix js

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,6 +1,10 @@
 (ns app.main)
 
-(defn increase []
-  (let [node (.querySelector js/document ".count_button")
-        count (js/parseInt (. node -innerText))]
-    (set! (. node -innerText) (+ 1 count))))
+(let [node (.querySelector js/document ".count_button")
+      count (js/parseInt (. node -innerText))]
+  (set! (. node -innerText) 0)
+  (set!
+   (.-onclick node)
+   (fn []
+     (let [count (js/parseInt (. node -innerText))]
+       (set! (. node -innerText) (+ 1 count))))))
